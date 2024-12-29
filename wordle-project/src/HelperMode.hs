b8d8ab29ad44463810756bbc7bd88b5a923f0537
Danail Todorov <danailtodorov440@gmail.com>
2024-12-29 19:03:11 +0200

    Update HelperMode.hs

--- wordle-project/src/HelperMode.hs
+++ wordle-project/src/HelperMode.hs
@@ -13,6 +13,7 @@ getInitialDictionary wordLength dict = [ word | word <- dict, length word == wor
 
 -- in the beggining the listOfColors is [] and the ind is 0
 -- returns the listOfColors [(ind / -1, (letter, color))], for example, for the current word
+
 getListOfColors :: Num t => [Char] -> [(t, (Char, String))] -> t -> Int -> IO [(t, (Char, String))]
 getListOfColors [] listOfColors _ _ = return listOfColors
 getListOfColors (x:xs) listOfColors ind wordLength
@@ -46,24 +47,24 @@ getWordsWithGreenLetters dict greenPairs = [word | word <- dict, all (\(ind, let
 
 -- words that do not contraverse the previous answers
 -- if the returned list is empty, then the player has lied
-getFilteredDict dict listOfColors = do
+getFilteredDict dict listOfColors =
     let greenPairs = [(ind, letter) | (ind, (letter, color)) <- listOfColors, color == "gr"]
         yellowLetters = [letter | (_, (letter, color)) <- listOfColors, color == "y"]
         initialGrayLetters = [letter | (_, (letter, color)) <- listOfColors, color == "g"]
         actualGrayLetters = removeNeededLetters initialGrayLetters yellowLetters []
-    putStrLn "printq zeleni dvoiki"
-    print greenPairs
-    putStrLn "printq julti dvoiki"
-    print yellowLetters
-    putStrLn "printq sivi dvoiki"
-    print actualGrayLetters
-    return (getWordsWithGreenLetters (getWordsWithYellowLetters (getWordsWithGrayLetters dict actualGrayLetters) yellowLetters) greenPairs)
+    in getWordsWithGreenLetters 
+            (getWordsWithYellowLetters 
+                (getWordsWithGrayLetters dict actualGrayLetters) 
+            yellowLetters) 
+        greenPairs
 
+removeNeededLetters :: (Foldable t, Eq a) => [a] -> t a -> [a] -> [a]
 removeNeededLetters [] _ newGrayLetters = newGrayLetters
-removeNeededLetters (x:restGray) yellowLetters newGrayList = if x `elem` yellowLetters 
+removeNeededLetters (x:restGray) yellowLetters newGrayList = if x `elem` yellowLetters
     then removeNeededLetters restGray yellowLetters newGrayList
     else removeNeededLetters restGray yellowLetters (x:newGrayList)
 
+getYellowLettersFromList :: (Eq a1, Num a2) => [a1] -> [(a1, a1)] -> [(a2, (a1, String))] -> [(a2, (a1, String))]
 getYellowLettersFromList _ [] lettersList = lettersList
 getYellowLettersFromList word ((f, s) : xs) lettersList = if f /= s && f `elem` word
     then getYellowLettersFromList (removeOccuranceFromActualWord word f) xs ((-1, (f, "y")) : lettersList)
@@ -80,42 +81,24 @@ getColorMatching offerWord secretWord =
 
 -- sums the removed words for a current word
 
-sumCoefficientForCurrentWord potentialOfferWord dict = do
-    let calculateRemovedWords secretWord = do
+sumCoefficientForCurrentWord potentialOfferWord dict =
+    let calculateRemovedWords secretWord =
             let colorMatchingList = getColorMatching potentialOfferWord secretWord
-            putStrLn "printq offer word"
-            print potentialOfferWord
-            putStrLn "printq secret word"
-            print secretWord
-            putStrLn "printq color maching list"
-            print colorMatchingList
-            filteredDict <- getFilteredDict dict colorMatchingList
-            return (length dict - length filteredDict)
-    results <- mapM calculateRemovedWords dict
-    return $ sum results
-    --sum (map calculateRemovedWords dict)
+                filteredDict = getFilteredDict dict colorMatchingList
+            in length dict - length filteredDict
+    in sum (map calculateRemovedWords dict)
 
-
-getBestWord :: (Show a2, Eq a2) => [[a2]] -> IO [a2]
-getBestWord dict = do
-    listOfPairsWordAndCoefficient <- mapM (\word -> do
-        coefficient <- sumCoefficientForCurrentWord word dict
-        return (word, coefficient)) dict
-    putStrLn "prntqt list ot dvoiki ot duma i koeficient"
-    print listOfPairsWordAndCoefficient
-    if null listOfPairsWordAndCoefficient
+getBestWord dict =
+    let listOfPairsWordAndCoefficient = [(word, sumCoefficientForCurrentWord word dict) | word <- dict]
+     -- reverse, so that I can return he first word with highest coefficient
+    in if null listOfPairsWordAndCoefficient
         then do
             putStrLn "\x1b[31mНякой от предишните ти отговори е бил лъжа!\x1b[0m"
             exitSuccess
-        else do
-            let bestWord = fst (maximumBy (comparing snd) listOfPairsWordAndCoefficient)
-            return bestWord
--- getBestWord dict = do
---     let listOfPairsWordAndCoefficient = [(word, sumCoefficientForCurrentWord word dict) | word <- dict]
---     print listOfPairsWordAndCoefficient
---         -- reverse, so that I can return he first word with highest coefficient
---     return (fst (maximumBy (comparing snd) listOfPairsWordAndCoefficient))
+        else
+            return (fst (maximumBy (comparing snd) listOfPairsWordAndCoefficient))
 
+            
 getWordFromPlayer :: Int -> IO String
 getWordFromPlayer wordLength = do
     putStr "Въведете дума по Ваш избор: "
@@ -127,7 +110,7 @@ getWordFromPlayer wordLength = do
         else
             return word
 
-evaluateGuessHelper :: [[Char]] -> Int -> IO ()
+
 evaluateGuessHelper dict wordLength = do
     bestWord <- getBestWord dict
     -- use pattern matching -> if the dict is empty, then head dict will throw an error!
@@ -136,16 +119,13 @@ evaluateGuessHelper dict wordLength = do
     if confirmationAnswer
         then do
             listOfColors <- getListOfColors bestWord [] 0 (length bestWord)
-            print listOfColors
             let listOfGreenLetters = [ letter | (_, (letter, color)) <- listOfColors, color == "gr"]
+                newDict = filter (\word -> word /= bestWord) dict
             if length listOfGreenLetters == wordLength
                 then
                     putStrLn ("Браво, позна думата: " ++ bestWord)
-                else do
-                    filteredDct <- getFilteredDict dict listOfColors
-                    putStrLn "tuk sym"
-                    print filteredDct
-                    evaluateGuessHelper filteredDct wordLength
+                else
+                    evaluateGuessHelper (getFilteredDict newDict listOfColors) wordLength
         else do
             word <- getWordFromPlayer wordLength
             listOfColors <- getListOfColors word [] 0 (length word)
@@ -154,10 +134,7 @@ evaluateGuessHelper dict wordLength = do
                 then
                     putStrLn ("Браво, позна думата: " ++ word)
                 else do
-                    filteredDct <- getFilteredDict dict listOfColors
-                    print filteredDct
-                    evaluateGuessHelper filteredDct wordLength
+                    evaluateGuessHelper (getFilteredDict dict listOfColors) wordLength
 
 startGameHelperMode dict = do
     evaluateGuessHelper dict (length (head dict))
-           
