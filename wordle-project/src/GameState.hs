92a2b7b848b7a6234daadde6037d946c8eeefd32
Danail Todorov <danailtodorov440@gmail.com>
2025-01-07 00:41:03 +0200

    Add files via upload

---
+++ wordle-project/src/GameState.hs
@@ -0,0 +1,47 @@
+module GameState ( updateGameState, GameState(greenLetters, yellowLetters, grayLetters), getAllFilters, emptyGameState) where
+
+import Data.List (nub)
+import Color ( Color(..) )
+import ColorUtils ( getColorMatching )
+
+type GrayLetters = [(Int, Char, Color)]
+type YellowLetters = [(Int, Char, Color)]
+type GreenLetters = [(Int, Char, Color)]
+
+data GameState = GameState {
+    grayLetters :: GrayLetters, 
+    yellowLetters :: YellowLetters, 
+    greenLetters :: GreenLetters
+    }
+
+getAllFilters :: GameState -> [(Int, Char, Color)]
+getAllFilters gameState = 
+    grayLetters gameState ++ 
+    yellowLetters gameState ++ 
+    greenLetters gameState
+
+emptyGameState :: GameState
+emptyGameState = GameState [] [] []
+
+filterLettersByColor :: Eq a1 => a1 -> [(a2, b, a1)] -> [(a2, b, a1)]
+filterLettersByColor color = filter (\(_, _, c) -> c == color)
+
+newGrayLettersList :: [(a2, b, Color)] -> [(a2, b, Color)]
+newGrayLettersList = filterLettersByColor Gray
+
+newYellowLettersList :: [(a2, b, Color)] -> [(a2, b, Color)]
+newYellowLettersList = filterLettersByColor Yellow
+
+newGreenLettersList :: [(a2, b, Color)] -> [(a2, b, Color)]
+newGreenLettersList =  filterLettersByColor Green
+
+updateGameState :: [Char] -> [Char] -> GameState -> GameState
+updateGameState guess actualWord gameState =
+    let colorMatchingPattern = getColorMatching guess actualWord
+    in GameState {
+        grayLetters = nub (grayLetters gameState ++ newGrayLettersList colorMatchingPattern),
+        yellowLetters = nub (yellowLetters gameState ++ newYellowLettersList colorMatchingPattern),
+        greenLetters = nub (greenLetters gameState ++ newGreenLettersList colorMatchingPattern)
+        }
+
+
