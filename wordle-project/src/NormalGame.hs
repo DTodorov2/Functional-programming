b614ba1f84e6fb5d4da6f936d9376a9095a29a7b
Danail Todorov <danailtodorov440@gmail.com>
2025-01-07 00:40:08 +0200

    Update NormalGame.hs

--- wordle-project/src/NormalGame.hs
+++ wordle-project/src/NormalGame.hs
@@ -1,56 +1,41 @@
-module NormalGame ( startGame, evaluateGuess, changeColor, colorTheLetters ) where
+module NormalGame ( startGame ) where
+
 import System.IO ( hFlush, stdout )
-import HelperMode ( getColorMatching )
-import Data.List (sortBy)
-import Data.Ord (comparing)
+import ColorUtils ( greenColor, whiteColor, redColor, colorTheLetters )
+import System.Exit ( exitSuccess )
 
 -- да направя ли да може да се exit-ва от играта, ако напише exit
 startGame :: String -> IO ()
-startGame actualWord = do
-    guess <- evaluateGuess actualWord (length actualWord)
-    if actualWord == guess
+startGame [] = do
+    putStrLn (redColor ++ "Думата трябва да има дължина поне 1" ++ whiteColor)
+    exitSuccess
+
+-- to pass path as an argument e get the index of a word from a random generator and that is how i pick the word
+startGame secretWord = do
+    guess <- evaluateGuess secretWord (length secretWord)
+    if secretWord == guess
         then do
-            putStrLn ("\x1b[32mБраво! Позна думата " ++ actualWord ++ "!\x1b[0m")
+            putStrLn ( greenColor ++ "Браво! Позна думата " ++ secretWord ++ whiteColor)
+            exitSuccess
         else
-            startGame actualWord 
+            startGame secretWord 
 
 evaluateGuess :: String -> Int -> IO String
+evaluateGuess [] _ = do
+    putStrLn (redColor ++ "Думата трябва да е с дължина поне 1!" ++ whiteColor)
+    exitSuccess
+
+evaluateGuess _ 0 = do 
+    putStrLn (redColor ++ "Дължината трябва да е поне 1!" ++ whiteColor)
+    exitSuccess
+
 evaluateGuess actualWord wordLength = do
     putStr "Въведи опит за познаване на думата: "
     hFlush stdout
     guess <- getLine
     if length guess /= wordLength
         then do
-            putStrLn ("\x1b[31mДумата трябва да бъде с дължина " ++ show wordLength ++ "\x1b[0m")
+            putStrLn ( redColor ++ "Думата трябва да бъде с дължина " ++ show wordLength ++ whiteColor)
             evaluateGuess actualWord wordLength
-            -- TO DO: To clear the screen after a difference in the length
         else do
             return (colorTheLetters guess actualWord)
-            
-
-colorTheLetters guess actualWord = 
-    let colorPattern = sortBy (comparing  (\(i, _, _) -> i)) (getColorMatching guess actualWord)
-    in concatMap (\(_, letter, color) -> changeColor color letter) colorPattern
-
--- правя низът в един цял стринг "\x1b[32m[letter]\x1b[0m\x1b[32m"[letter]\x1b[0m\x1b[32m[letter]\x1b[0m"
--- от този тип и в горната функция го печатам
--- colorTheLetters :: [Char] -> [Char] -> [Char]
--- colorTheLetters guess actualWord = helper guess actualWord "" actualWord where
---     helper [] [] result _ = result -- two of the lists are empty -> return result
---     helper [] _ result _ = result
---     helper _ [] result _ = result
---     helper (x:xs) (y:ys) result contains
---         | x == y        = helper xs ys (result ++ changeColor "green" x) (removeOccuranceFromActualWord contains x)
---         | x `elem` contains   = helper xs ys (result ++ changeColor "yellow" x) (removeOccuranceFromActualWord contains x)
---         | otherwise     = helper xs ys (result ++ changeColor "gray" x) contains
-
--- removeOccuranceFromActualWord :: Eq t => [t] -> t -> [t]
--- removeOccuranceFromActualWord [] _ = []
--- removeOccuranceFromActualWord (x:xs) letter = if x == letter then xs else x : removeOccuranceFromActualWord xs letter
-
--- to say that it is from chatGpt
-changeColor :: String -> Char -> [Char]
-changeColor wantedColor letter
-    | wantedColor == "gr"     = "\x1b[32m" ++ [letter] ++ "\x1b[0m"
-    | wantedColor == "y"    = "\x1b[33m" ++ [letter] ++ "\x1b[0m"
-    | otherwise                  = "\x1b[90m" ++ [letter] ++ "\x1b[0m"
