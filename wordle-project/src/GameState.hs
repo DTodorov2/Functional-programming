380ffec777a24b5fc8ca007619c9ca27b1954f3e
Danail Todorov <danailtodorov440@gmail.com>
2025-01-07 19:48:11 +0200

    Update GameState.hs

--- wordle-project/src/GameState.hs
+++ wordle-project/src/GameState.hs
@@ -1,4 +1,4 @@
-module GameState ( updateGameState, GameState(greenLetters, yellowLetters, grayLetters), getAllFilters, emptyGameState) where
+module GameState ( getFiltersCount, updateGameState, GameState(greenLetters, yellowLetters, grayLetters), getAllFilters, emptyGameState) where
 
 import Data.List (nub)
 import Color ( Color(..) )
@@ -9,15 +9,15 @@ type YellowLetters = [(Int, Char, Color)]
 type GreenLetters = [(Int, Char, Color)]
 
 data GameState = GameState {
-    grayLetters :: GrayLetters, 
-    yellowLetters :: YellowLetters, 
+    grayLetters :: GrayLetters,
+    yellowLetters :: YellowLetters,
     greenLetters :: GreenLetters
     }
 
 getAllFilters :: GameState -> [(Int, Char, Color)]
-getAllFilters gameState = 
-    grayLetters gameState ++ 
-    yellowLetters gameState ++ 
+getAllFilters gameState =
+    grayLetters gameState ++
+    yellowLetters gameState ++
     greenLetters gameState
 
 emptyGameState :: GameState
@@ -44,4 +44,5 @@ updateGameState guess actualWord gameState =
         greenLetters = nub (greenLetters gameState ++ newGreenLettersList colorMatchingPattern)
         }
 
-
+getFiltersCount gameState =
+    length (nub [ letter | (_, letter, _) <- yellowLetters gameState ++ greenLetters gameState])
